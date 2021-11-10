04b9206146e5a54a434f99e3c0f2dccb70bc807b
jknights2000 <56910576+jknights2000@users.noreply.github.com>
2021-11-10 11:09:08 +0000

    add simple multipler

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('welcome to calculator!');
+
+const readline = require("readline-sync");
+
+console.log('Enter Number 1:');
+const reponse1 = readline.prompt();
+console.log('Enter Number 2:');
+const reponse2 = readline.prompt();
+
+const result = reponse1 * reponse2;
+
+console.log(`${reponse1} x ${reponse2} = ${result}`);
+
+
