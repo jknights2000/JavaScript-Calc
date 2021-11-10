9e575b11371a43621499c8ede59060b770a2e42b
jknights2000 <56910576+jknights2000@users.noreply.github.com>
2021-11-10 11:39:38 +0000

    array

--- index.js
+++ index.js
@@ -3,32 +3,60 @@ console.log('welcome to calculator!');
 const readline = require("readline-sync");
 console.log('operator:');
 const op = readline.prompt();
-console.log('Enter Number 1:');
-const number1 = readline.prompt();
-console.log('Enter Number 2:');
-const number2 = readline.prompt();
-let result = 0;
-switch(op){
-    case "*":
-        result = number1 * number2;
-        console.log(`${number1} x ${number2} = ${result}`);
-        break;
-    case "+":
-        result = parseInt(number1) + parseInt(number2);
-        console.log(`${number1} + ${number2} = ${result}`);
-        break;
-    case "/":
-        result = number1 / number2;
-        console.log(`${number1} / ${number2} = ${result}`);
-        break;
-    case "-":
-        result = number1 - number2;
-        console.log(`${number1} - ${number2} = ${result}`);
-        break;
-    default:
-        console.log(`invalid operator`);
-        break;
+
+console.log(`number of numbers to ${op}:`);
+const amount = readline.prompt();
+let arr = new Array(amount);
+
+for(let i = 0; i < amount;i++){
+    let counter = i + 1;
+    console.log(`Enter Number ${counter}:`);
+    let number = readline.prompt();
+    arr[i] = number;
 }
+
+let result = 0;
+
+for(let i = 0; i < amount;i++){
+
+    switch(op){
+        case "*":
+            if(result==0)
+            {
+                result = arr[i];
+            }else
+            {
+                result = result * arr[i];
+            }
+            
+            break;
+        case "+":
+            result = result + parseInt(arr[i]);
+            break;
+        case "/":
+            if(result==0)
+            {
+                result = arr[i];
+            }else
+            {
+                result = result / arr[i];
+            }
+            break;
+        case "-":
+            if(result==0)
+            {
+                result = arr[i];
+            }else
+            {
+                result = result - arr[i];
+            }
+            break;
+        default:
+            console.log(`invalid operator`);
+            break;
+    }
+}
+console.log(`Answer = ${result}`);
 /*
 if("*" == op){
     const result = number1 * number2;
