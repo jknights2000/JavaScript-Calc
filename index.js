e5a389460fbbeb38bc9391581fcdc078c1b9f781
jknights2000 <56910576+jknights2000@users.noreply.github.com>
2021-11-10 11:25:20 +0000

    switch statement version

--- index.js
+++ index.js
@@ -7,6 +7,29 @@ console.log('Enter Number 1:');
 const number1 = readline.prompt();
 console.log('Enter Number 2:');
 const number2 = readline.prompt();
+let result = 0;
+switch(op){
+    case "*":
+        result = number1 * number2;
+        console.log(`${number1} x ${number2} = ${result}`);
+        break;
+    case "+":
+        result = parseInt(number1) + parseInt(number2);
+        console.log(`${number1} + ${number2} = ${result}`);
+        break;
+    case "/":
+        result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        break;
+    case "-":
+        result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        break;
+    default:
+        console.log(`invalid operator`);
+        break;
+}
+/*
 if("*" == op){
     const result = number1 * number2;
 
@@ -28,6 +51,6 @@ if("*" == op){
 
     console.log(`invalid operator`);
 }
-
+*/
 
 
